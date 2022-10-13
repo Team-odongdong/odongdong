452a4883d08c470f738f06c1e3bc8c372d5b745e
jhbaik1501 <81180977+jhbaik1501@users.noreply.github.com>
2022-10-13 23:01:32 +0900

    Hotfix: import 오류

--- src/backend/src/main/java/com/graduate/odondong/controller/InqueryController.java
+++ src/backend/src/main/java/com/graduate/odondong/controller/InqueryController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
