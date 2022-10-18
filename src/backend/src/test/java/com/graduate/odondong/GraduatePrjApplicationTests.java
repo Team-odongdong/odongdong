ff95ef550f4694c5e98e776445bef76633cb0244
jhbaik1501 <jhbaik1501@gmail.com>
2022-10-18 22:58:47 +0900

    fix: test코드 전부 제거

--- src/backend/src/test/java/com/graduate/odondong/GraduatePrjApplicationTests.java
+++ src/backend/src/test/java/com/graduate/odondong/GraduatePrjApplicationTests.java
@@ -1,51 +1,38 @@
 package com.graduate.odondong;
 
-import com.graduate.odondong.domain.Bathroom;
-import com.graduate.odondong.dto.CoordinateInfoDto;
-import com.graduate.odondong.service.BathroomService.BathroomService;
-import com.graduate.odondong.service.BathroomService.BulkInsertPublicBathroom;
-import com.graduate.odondong.util.ReverseGeocoding.ChangeByGeocoderKakao;
-import com.graduate.odondong.util.ReverseGeocoding.ChangeByGeocoderNaver;
-import com.graduate.odondong.util.ReverseGeocoding.ReverseGeocoding;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.time.LocalTime;
-import java.util.List;
 
 @SpringBootTest
 class GraduatePrjApplicationTests {
 
-	@Autowired
-	ChangeByGeocoderKakao changeByGeocoder;
-	@Autowired
-	BulkInsertPublicBathroom bulkInsertPublicBathroom;
-	@Autowired
-	BathroomService bathroomService;
-
-	@Autowired
-	ChangeByGeocoderNaver changeByGeocoderNaver;
-
-	@Test
-	void 테스트() {
-		List<Bathroom> kmByLongitudeLatitude = bathroomService.get1kmByLongitudeLatitude(126.966182351829, 37.5911968710618);
-		return;
-	}
-	
-	@Test
-	void 테스트2() {
-		bulkInsertPublicBathroom.BulkInsert("12_04_01_E_공중화장실정보.xlsx");
-	}
-
-	@Test
-	void naver지오코딩테스트() {
-		System.out.println(LocalTime.now());
-		CoordinateInfoDto addressByCoordinate1 = changeByGeocoder.getAddressByCoordinate(126.91298945203502,37.574952579149084 );
-		System.out.println(LocalTime.now());
-		CoordinateInfoDto addressByCoordinate = changeByGeocoderNaver.getAddressByCoordinate(126.91298945203502,37.574952579149084 );
-		System.out.println(LocalTime.now());
-		return;
-	}
+//	@Autowired
+//	ChangeByGeocoderKakao changeByGeocoder;
+//	@Autowired
+//	BulkInsertPublicBathroom bulkInsertPublicBathroom;
+//	@Autowired
+//	BathroomService bathroomService;
+//
+//	@Autowired
+//	ChangeByGeocoderNaver changeByGeocoderNaver;
+//
+//	@Test
+//	void 테스트() {
+//		List<Bathroom> kmByLongitudeLatitude = bathroomService.get1kmByLongitudeLatitude(126.966182351829, 37.5911968710618);
+//		return;
+//	}
+//
+//	@Test
+//	void 테스트2() {
+//		bulkInsertPublicBathroom.BulkInsert("12_04_01_E_공중화장실정보.xlsx");
+//	}
+//
+//	@Test
+//	void naver지오코딩테스트() {
+//		System.out.println(LocalTime.now());
+//		CoordinateInfoDto addressByCoordinate1 = changeByGeocoder.getAddressByCoordinate(126.91298945203502,37.574952579149084 );
+//		System.out.println(LocalTime.now());
+//		CoordinateInfoDto addressByCoordinate = changeByGeocoderNaver.getAddressByCoordinate(126.91298945203502,37.574952579149084 );
+//		System.out.println(LocalTime.now());
+//		return;
+//	}
 }
