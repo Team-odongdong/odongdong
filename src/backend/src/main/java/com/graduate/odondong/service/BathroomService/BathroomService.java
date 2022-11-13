23d5dc2276c7c8acc8e84b42bafe74099235d545
wonseok <tkddls23@konkuk.ac.kr>
2022-11-13 10:42:18 +0900

    refactor: 개방시간 유무 판별 기능 클래스 분리

--- src/backend/src/main/java/com/graduate/odondong/service/BathroomService/BathroomService.java
+++ src/backend/src/main/java/com/graduate/odondong/service/BathroomService/BathroomService.java
@@ -11,6 +11,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.graduate.odondong.util.operationTime.OperationTimeValidation;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -39,6 +40,7 @@ public class BathroomService {
     private final ChangeByGeocoderKakao changeByGeocoderKakao;
     private final ChangeByGeocoderNaver changeByGeocoderNaver;
     private final DeletedBathroomService deletedBathroomService;
+    private final OperationTimeValidation operationTimeValidation;
 
     public List<BathroomResponseInterface> bathroomList() {
         return bathroomRepository.findAllBathroomResponseDto();
@@ -113,7 +115,7 @@ public class BathroomService {
                     .isLocked(data.getIsLocked())
                     .register(data.getRegister())
                     .isUnisex(data.getIsUnisex())
-                    .isOpened(checkBathroomOpen(data.getOperationTime()))
+                    .isOpened(operationTimeValidation.checkBathroomOpen(data.getOperationTime()))
                     .build()
             ).collect(Collectors.toList());
             return new BaseResponse<>(bathroomResponseDtos);
@@ -133,47 +135,5 @@ public class BathroomService {
             throw new BaseException(DATABASE_ERROR);
         }
 
-    }
-
-    public String checkBathroomOpen(String operation){
-        SimpleDateFormat inputFormat = new SimpleDateFormat("HH:mm");
-        LocalDateTime now = LocalDateTime.now();
-        int dayOfWeekValue = now.getDayOfWeek().getValue();
-        String nowTime = now.getHour() + ":" + now.getMinute();
-        Date nowDateTime = null;
-
-        try {
-            nowDateTime = inputFormat.parse(nowTime);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-
-        if (operation.equals("24시간")) {
-            return "Y";
-        }
-        if (operation.contains("평일")) {
-            if (dayOfWeekValue == 6 || dayOfWeekValue == 7) {
-                return "N";
-            }
-            return "Y";
-        }
-
-        if (operation.length() == 11) {
-            String[] operationTime = operation.split("~");
-            Date startDateTime = null;
-            Date endDateTime = null;
-            try {
-                startDateTime = inputFormat.parse(operationTime[0]);
-                endDateTime = inputFormat.parse(operationTime[1]);
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
-            if (nowDateTime.before(startDateTime) || nowDateTime.after(endDateTime)) {
-                return "N";
-            }
-            return "Y";
-        }
-
-        return "Y";
     }
 }
