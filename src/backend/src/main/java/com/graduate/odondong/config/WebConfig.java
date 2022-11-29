303961de8f2678f2734f42c2e6acc95c0daaa667
jhbaik1501 <jhbaik1501@gmail.com>
2022-11-29 19:09:27 +0900

    fix: webconfig change

--- src/backend/src/main/java/com/graduate/odondong/config/WebConfig.java
+++ src/backend/src/main/java/com/graduate/odondong/config/WebConfig.java
@@ -7,13 +7,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    public static final String ServerBaseUrl = "127.0.0.1:9003";
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "OPTIONS");
-    }
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOrigins("http://localhost:9003", "http://localhost:3000", "http://127.0.0.1:3000", "https://odongdong.site"
+			,"http://localhost:8081", "http://localhost:8100", "http://localhost:8000")
+			.allowedMethods("GET", "POST", "OPTIONS")
+			.allowCredentials(true);
+	}
 
 }
