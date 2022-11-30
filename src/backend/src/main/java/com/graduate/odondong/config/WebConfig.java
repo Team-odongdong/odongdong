bcc9cdf423b8bb7b25004262985dc59dfd4251cd
jhbaik1501 <jhbaik1501@gmail.com>
2022-11-30 15:01:19 +0900

    fix: allow할 Origin을 odongdong.site와 localhost:8100 두개로 변경

--- src/backend/src/main/java/com/graduate/odondong/config/WebConfig.java
+++ src/backend/src/main/java/com/graduate/odondong/config/WebConfig.java
@@ -10,8 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins("http://localhost:9003", "http://localhost:3000", "http://127.0.0.1:3000", "http://127.0.0.1:3000")
-			.allowedMethods("*")
+			.allowedOrigins("https://odongdong.site", "http://localhost:8100")
+			.allowedMethods("GET", "POST", "OPTIONS")
 			.allowCredentials(true);
 	}
 
