8b7cb0e8eaada148ff5f72bd05253149007a851a
msdio <rlagofka1@naver.com>
2022-12-20 18:00:31 +0900

    [chore] version 1.3.0

--- src/frontend/odongdong/src/environments/environment.ts
+++ src/frontend/odongdong/src/environments/environment.ts
@@ -3,19 +3,19 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  appVersion: {
-    name: '1.1.5',
-    major: 1,
-    minor: 1,
-    build: 5,
-  },
+    production: false,
+    appVersion: {
+        name: '1.3.0',
+        major: 1,
+        minor: 3,
+        build: 0,
+    },
 
-  // apiUrl: 'http://localhost:9003',
-  // apiUrl: 'http://13.125.245.177:9003',
-  // apiUrl: 'http://odongdong.site:9003',
-  // apiUrl: 'https://odongdong.site',
-  apiUrl: 'https://prod.odongdong.site',
+    // apiUrl: 'http://localhost:9003',
+    // apiUrl: 'http://13.125.245.177:9003',
+    // apiUrl: 'http://odongdong.site:9003',
+    // apiUrl: 'https://odongdong.site',
+    apiUrl: 'https://prod.odongdong.site',
 };
 
 /*
